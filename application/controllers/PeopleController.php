28c71f8e0dcf71bd34ec970c98f01a3920bc99fb
rpariona81 <ronald.pariona.isidro@gmail.com>
2019-12-23 23:07:30 -0500

    helper my_serializer activado

--- application/controllers/PeopleController.php
+++ application/controllers/PeopleController.php
@@ -1,23 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-
 
 class PeopleController extends CI_Controller
 {
 
-    var $encoders;
-    var $normalizers;
-    var $serializer;
     public function __construct()
     {
         parent::__construct();
         $this->load->model('People_model','People_model');
-        $this->encoders = [new XmlEncoder(), new JsonEncoder()];
-        $this->normalizers = [new ObjectNormalizer()];
-        $this->serializer = new Serializer($this->normalizers, $this->encoders);
     }
 
     //--------------------------------
@@ -27,7 +16,7 @@ class PeopleController extends CI_Controller
         $data['titulo'] = 'CodeIgniter 3 + Doctrine 2.3.3 ';
         $data['entradas'] = $this->People_model->get_people();
         //echo json_encode($data['entradas']);
-        $this->load->view('entradas_view', $data);
+        $this->load->view('people_view', $data);
     }
 
     //--------------------------------------
--- application/controllers/PostController.php
+++ application/controllers/PostController.php
@@ -1,26 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-use Doctrine\ORM\Mapping\Entity;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-
-
 class PostController extends CI_Controller
 {
 
-    var $encoders;
-    var $normalizers;
-    var $serializer;
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Posts_model','Posts_model');
-        $this->encoders = [new XmlEncoder(), new JsonEncoder()];
-        $this->normalizers = [new ObjectNormalizer()];
-        $this->serializer = new Serializer($this->normalizers, $this->encoders);
-        $this->load->helper('my_serializer');
     }
 
     //--------------------------------
